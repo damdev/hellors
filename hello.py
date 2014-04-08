ee334cb4dbe5cffcc3232de37fe65c48f91d6ac6
Damian Franetovich <dfranetovich@despegar.com>
2014-04-08 12:13:17 -0300

    add hola support

--- hello.py
+++ hello.py
@@ -1,7 +1,13 @@
+# -*- coding: utf-8 -*-
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/hello/<name>')
 def hello(name):
-  return "Hello %s!" % name
+    return "Hello %s!" % name
+
+@app.route('/hola/<nombre>')
+def hola(nombre):
+    return u"¡Hola %s!" % nombre
