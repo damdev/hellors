b9d15644e0484c80f51240ea15f454475907bc89
Damian Franetovich <dfranetovich@despegar.com>
2014-04-09 16:27:31 -0300

    allow cross origin domain requests

--- hello.py
+++ hello.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask
+from crossdomain import crossdomain
 
 app = Flask(__name__)
 
 @app.route('/hello/<name>')
+@crossdomain(origin='*')
 def hello(name):
     return "Hello %s!" % name
 
 @app.route('/hola/<nombre>')
+@crossdomain(origin='*')
 def hola(nombre):
     return u"¡Hola %s!" % nombre
